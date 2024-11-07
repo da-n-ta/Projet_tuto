392711fa55ac7f7799428e202f0fbc46ddb2a289
da-n-ta <73463092+da-n-ta@users.noreply.github.com>
2024-11-07 17:59:11 +0100

    donnees a utiliser et reunion

---
+++ donnees.R
@@ -0,0 +1,3 @@
+mens <- read.csv("~/Downloads/Q_38_previous-1950-2022_RR-T-Vent.csv/Q_38_previous-1950-2022_RR-T-Vent.csv", header=FALSE, sep=";")
+names()
+#faire de petite bases de donnees pour chacune des stations et de la stat descriptives
