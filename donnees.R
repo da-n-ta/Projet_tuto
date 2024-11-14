f5e8677b86429d4489cd0fc3e04fd53b84093b04
da-n-ta <73463092+da-n-ta@users.noreply.github.com>
2024-11-14 13:55:02 +0100

    Update donnees.R

--- donnees.R
+++ donnees.R
@@ -1,3 +1,18 @@
-mens <- read.csv("~/Downloads/Q_38_previous-1950-2022_RR-T-Vent.csv/Q_38_previous-1950-2022_RR-T-Vent.csv", header=FALSE, sep=";")
-names()
+library(dplyr)
+quot <- read.csv("~/Downloads/Q_38_previous-1950-2022_RR-T-Vent.csv/Q_38_previous-1950-2022_RR-T-Vent.csv", header=FALSE, sep=";")
+
 #faire de petite bases de donnees pour chacune des stations et de la stat descriptives
+
+colnames(quot) <- as.character(unlist(quot[1, ]))
+quot <- quot[-1, ]
+typeof(quot$RR)
+
+quot$RR <- as.numeric(quot$RR)
+quot$NUM_POSTE <- as.numeric(quot$NUM_POSTE)
+quot$AAAAMMJJ <- as.numeric(quot$AAAAMMJJ)
+
+
+
+data_per_station <- split(quot,quot$NOM_USUEL)
+
+data_per_station$`AIGLETON-NIVOSE`$AAAAMMJJ
